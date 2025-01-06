6aae4b6e17298d8bf5afaaae5e2ea6083f587f58
antuntun <antunmilakovic@gmail.com>
2025-01-06 23:33:57 +0100

    added libraries and some methods to code

--- OsjetniModul/Kod/OsjetniModul/src/main.cpp
+++ OsjetniModul/Kod/OsjetniModul/src/main.cpp
@@ -1,4 +1,7 @@
 #include <Arduino.h>
+#include <SoftwareSerial.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
 int dsTempPin = 8; // DS18B20 Temperature Sensor
 
@@ -9,8 +12,10 @@ int cap4Pin = 5; // Capacitive Sensor 4
 int cap5Pin = 6; // Capacitive Sensor 5
 int cap6Pin = 7; // Capacitive Sensor 6
 
-int softwareSerialRx = 9; // Software Serial RX -> MAX485 RO
-int softwareSerialTx = 11; // Software Serial TX -> MAX485 DI
+int RX_PIN = 9; // Software Serial RX -> MAX485 RO
+int TX_PIN = 11; // Software Serial TX -> MAX485 DI
+
+SoftwareSerial rs485Serial(RX_PIN, TX_PIN);
 
 int fotoSensorPin = A0; // Foto Sensor
 
@@ -18,20 +23,63 @@ int KTypeMiso = A5; // K-Type Thermocouple MISO
 int KTypeCs = A6; // K-Type Thermocouple CS
 int KTypeSck = A7; // K-Type Thermocouple SCK
 
+int ds1Temp = 0;
+int ds2Temp = 0;
+int ds3Temp = 0;
+int ds4Temp = 0; // TODO: maybe not needed
 
-// put function declarations here:
-int myFunction(int, int);
+int cap1State = 0;
+int cap2State = 0;
+int cap3State = 0;
+int cap4State = 0;
+int cap5State = 0;
+int cap6State = 0;
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+int fotoSensorValue = 0;
+int kTypeTemp = 0;
+
+char[] dataToSend = new char[64];
+void CreateReply(int ds1Temp, int ds2Temp, int ds3Temp, int ds4Temp, int cap1State, int cap2State, int cap3State, int cap4State, int cap5State, int cap6State, int fotoSensorValue, int kTypeTemp)
+int ReadDsSensor(int sensorPin);
+void setup() 
+{
+  rs485Serial.begin(9600);
 }
 
-void loop() {
+void loop() 
+{
   // put your main code here, to run repeatedly:
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+
+
+void CreateReply(int ds1Temp, int ds2Temp, int ds3Temp, int ds4Temp, int cap1State, int cap2State, int cap3State, int cap4State, int cap5State, int cap6State, int fotoSensorValue, int kTypeTemp)
+{
+  rs485Serial.Write(xD1);
+  rs485Serial.Write(ds1Temp);
+  rs485Serial.Write(0xD2);
+  rs485Serial.Write(ds2Temp);
+  rs485Serial.Write(0xD3);
+  rs485Serial.Write(ds3Temp);
+  rs485Serial.Write(0xD4);
+  rs485Serial.Write(ds4Temp);
+
+  rs485Serial.Write(0xC1);
+  rs485Serial.Write(cap1State == HIGH ? 1 : 0);
+  rs485Serial.Write(0xC2);
+  rs485Serial.Write(cap2State == HIGH ? 1 : 0);
+  rs485Serial.Write(0xC3);
+  rs485Serial.Write(cap3State == HIGH ? 1 : 0);
+  rs485Serial.Write(0xC4);
+  rs485Serial.Write(cap4State == HIGH ? 1 : 0);
+  rrs485Serial.Write(0xC5);
+  rs485Serial.Write(cap5State == HIGH ? 1 : 0);
+  rs485Serial.Write(0xC6);
+  rs485Serial.Write(cap6State == HIGH ? 1 : 0);
+
+  rs485Serial.Write(0xF1);
+  rs485Serial.Write(fotoSensorValue);
+
+  rs485Serial.Write(0xE1); // K-Type Thermocouple
+  rs485Serial.Write(kTypeTemp);
 } 
